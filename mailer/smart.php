1441451cadf8f9189e092a5690d6e0616349a1bf
Azmodan13 <96807930+Azmodan13@users.noreply.github.com>
2022-03-23 18:33:15 +0200

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'cfsangvinik@gmail.com';                 // Наш логин
-$mail->Password = 'rzbxzagihdigvztm';                           // Наш пароль от ящика ПАРОЛЬ ДЛЯ ПРИЛОЖЕНИЙ
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика ПАРОЛЬ ДЛЯ ПРИЛОЖЕНИЙ
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('cfsangvinik@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('moxide6372@minimeq.com');     // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
